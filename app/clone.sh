9e0177042c416a17f37cea893d47693c0b02f6af
mbelengonzalez168 <138723897+mbelengonzalez168@users.noreply.github.com>
2024-04-02 13:42:35 -0300

    Update clone.sh

--- app/clone.sh
+++ app/clone.sh
@@ -4,6 +4,5 @@ REPOSITORIO=$2
 rm -rf /opt/framework || true
 mkdir -p /opt/framework
 git clone -b $RAMA $REPOSITORIO /opt/framework
-[13:39] Richard Chamorro
 chmod 755 /opt/framework
 cd /opt/framework/
